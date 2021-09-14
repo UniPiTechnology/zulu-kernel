c6ee2fd59649b62651a55bac8bca95e583fea418
Miroslav Ondra <ondra@faster.cz>
2021-09-14 17:19:50 +0200

    Remove device-tree overlays. Refactor rules

--- gitlab-ci/build-before-script.sh
+++ gitlab-ci/build-before-script.sh
@@ -3,3 +3,10 @@
 # https://stackoverflow.com/questions/5750450/how-can-i-print-each-command-before-executing
 set -o xtrace
 apt-get -y install bc libssl-dev
+
+# modify repo-patch-table
+
+cat >/ci-scripts/repo_patch_table.txt <<EOF
+buster-zulu-main      buster-zulu-main bullseye-zulu-main  bullseye-patron-main  bullseye-iris-main
+buster-zulu-test      buster-zulu-test bullseye-zulu-test  bullseye-patron-test  bullseye-iris-test
+EOF
