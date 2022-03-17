e63483cb64768c2678e8f43516aad1e41b5f6294
Martin Kudlacek <kudlacek@unipi.technology>
2022-03-17 13:18:28 +0100

    Splitting the package names for bullseye onwards

--- gitlab-ci/build-before-script.sh
+++ gitlab-ci/build-before-script.sh
@@ -4,8 +4,20 @@
 set -o xtrace
 apt-get -y install bc libssl-dev
 
-# modify repo-patch-table
+. /ci-scripts/include.sh
 
+# the debian/control needs to be altered for older distros
+if [ "$DEBIAN_VERSION" == "buster" ] || [ "$DEBIAN_VERSION" == "stretch" ]; then
+    sed -i 's/###KERNEL_PACKAGE_NAME###/zulu-kernel-image/g' debian/control
+    sed -i 's/###HEADERS_PACKAGE_NAME###/zulu-kernel-headers/g' debian/control
+    sed -i 's/###DEPENDS###/zulu-u-boot (>=0.34)/g' debian/control
+else
+    sed -i 's/###KERNEL_PACKAGE_NAME###/unipi-kernel-image/g' debian/control
+    sed -i 's/###HEADERS_PACKAGE_NAME###/unipi-kernel-headers/g' debian/control
+    sed -i 's/###DEPENDS###/unipi-u-boot (>=0.34)/g' debian/control
+fi
+
+# modify repo-patch-table
 cat >/ci-scripts/repo_patch_table.txt <<EOF
 bullseye-zulu-main    bullseye-zulu-main  bullseye-patron-main  bullseye-iris-main
 bullseye-zulu-test    bullseye-zulu-test  bullseye-patron-test  bullseye-iris-test buster-zulu-test
